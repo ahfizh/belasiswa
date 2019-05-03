ca1af3b7c773d0533f4421f6c0357406ff6b5bc2
Alfiansyah Nur Zufa <anurzufa@gmail.com>
2019-05-04 05:46:28 +0700

    konoyon- bottomnavigation rev2

--- app/src/main/java/com/ujuy/ahfizh/belasiswa/MainActivity.java
+++ app/src/main/java/com/ujuy/ahfizh/belasiswa/MainActivity.java
@@ -1,50 +1,62 @@
 package com.ujuy.ahfizh.belasiswa;
 
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.ujuy.ahfizh.belasiswa.activity.fragment.FragmentHome;
+import com.ujuy.ahfizh.belasiswa.activity.fragment.FragmentProfile;
+import com.ujuy.ahfizh.belasiswa.activity.fragment.FragmentTask;
+
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadFragment(new FragmentHome());
+
+        BottomNavigationView navigation = findViewById(R.id.bn_main);
+        navigation.setOnNavigationItemSelectedListener(this);
     }
 
-    private void initBottomViewAndLoadFragments(final BottomNavigationViewEx bnve) {
-        bnve.enableAnimation(false);
-        bnve.enableShiftingMode(false);
-        bnve.enableItemShiftingMode(false);
 
-        // use the unchecked color for first item
-        bnve.setIconTintList(0, getResources().getColorStateList(R.color.bnv_unchecked_black));
-        bnve.setTextTintList(0, getResources().getColorStateList(R.color.bnv_unchecked_black));
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
 
-        bnve.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        switch (item.getItemId()) {
+            case R.id.naviigation_home:
+                fragment = new FragmentHome();
+                break;
 
-            private boolean firstClick = true;
-            private int lastItemId = -1;
+            case R.id.navigation_task:
+                fragment = new FragmentTask();
+                break;
+            case R.id.navigation_profile:
+                fragment = new FragmentProfile();
+                break;
 
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                // restore the color when click
-                if (firstClick) {
-                    firstClick = false;
-                    bnve.setIconTintList(0, getResources().getColorStateList(R.color.selector_bnv));
-                    bnve.setTextTintList(0, getResources().getColorStateList(R.color.selector_bnv));
-                }
+        }
 
-                if (firstClick || lastItemId == -1 || lastItemId != item.getItemId()) {
-                    lastItemId = item.getItemId();
-                } else {
-                    return false;
-                }
-
-                // do stuff
-                return fillContent(item.getItemId());
-            }
-        });
+        return loadFragment(fragment);
+    }
+    //kelas pinjam parkiran jadwal
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fl_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
