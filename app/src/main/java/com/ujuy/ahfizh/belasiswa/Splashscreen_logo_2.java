32ef6eb62eb261235323aa6b96697137a7f5bd81
AHFIZH <iceprincelf@gmail.com>
2019-05-03 15:40:30 +0700

    Initial commit

---
+++ app/src/main/java/com/ujuy/ahfizh/belasiswa/Splashscreen_logo_2.java
@@ -0,0 +1,13 @@
+package com.ujuy.ahfizh.belasiswa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splashscreen_logo_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen_logo_2);
+    }
+}
