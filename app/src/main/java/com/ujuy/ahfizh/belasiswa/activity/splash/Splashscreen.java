672355a1ce8e26d02707de1c475290fd20fcaef8
ahfizh <iceprincelf@gmail.com>
2019-05-03 23:23:14 +0700

    ujuy- tambah mainactivity.java

--- app/src/main/java/com/ujuy/ahfizh/belasiswa/activity/splash/Splashscreen.java
+++ app/src/main/java/com/ujuy/ahfizh/belasiswa/activity/splash/Splashscreen.java
@@ -9,7 +9,7 @@ import android.widget.ImageView;
 import com.ujuy.ahfizh.belasiswa.R;
 import com.ujuy.ahfizh.belasiswa.activity.intro.Splashscreen_logo;
 
-public class MainActivity extends AppCompatActivity {
+public class Splashscreen extends AppCompatActivity {
     // ini untuk splashscreen
     // konyon text
     private int time=3000;
@@ -19,12 +19,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splashscreen);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this, Splashscreen_logo.class);
+                Intent intent = new Intent(Splashscreen.this, Splashscreen_logo.class);
                 startActivity(intent);
                 finish();
             }
