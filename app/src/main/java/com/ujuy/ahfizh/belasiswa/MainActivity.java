8b582a3e3fb073864c0da938e38d15bd447b6c03
Alfiansyah Nur Zufa <anurzufa@gmail.com>
2019-05-04 05:29:38 +0700

    konoyon-bottomnavigation

--- app/src/main/java/com/ujuy/ahfizh/belasiswa/MainActivity.java
+++ app/src/main/java/com/ujuy/ahfizh/belasiswa/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ujuy.ahfizh.belasiswa;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void initBottomViewAndLoadFragments(final BottomNavigationViewEx bnve) {
+        bnve.enableAnimation(false);
+        bnve.enableShiftingMode(false);
+        bnve.enableItemShiftingMode(false);
+
+        // use the unchecked color for first item
+        bnve.setIconTintList(0, getResources().getColorStateList(R.color.bnv_unchecked_black));
+        bnve.setTextTintList(0, getResources().getColorStateList(R.color.bnv_unchecked_black));
+
+        bnve.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+            private boolean firstClick = true;
+            private int lastItemId = -1;
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                // restore the color when click
+                if (firstClick) {
+                    firstClick = false;
+                    bnve.setIconTintList(0, getResources().getColorStateList(R.color.selector_bnv));
+                    bnve.setTextTintList(0, getResources().getColorStateList(R.color.selector_bnv));
+                }
+
+                if (firstClick || lastItemId == -1 || lastItemId != item.getItemId()) {
+                    lastItemId = item.getItemId();
+                } else {
+                    return false;
+                }
+
+                // do stuff
+                return fillContent(item.getItemId());
+            }
+        });
     }
 }
