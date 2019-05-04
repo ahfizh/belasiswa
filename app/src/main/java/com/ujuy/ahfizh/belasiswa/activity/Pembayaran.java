72a8ff4909afd854206d570789ed52e5402be3b1
ahfizh <iceprincelf@gmail.com>
2019-05-04 16:35:54 +0700

    ujuy- riwayat donasi

---
+++ app/src/main/java/com/ujuy/ahfizh/belasiswa/activity/Pembayaran.java
@@ -0,0 +1,15 @@
+package com.ujuy.ahfizh.belasiswa.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ujuy.ahfizh.belasiswa.R;
+
+public class Pembayaran extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pembayaran);
+    }
+}
